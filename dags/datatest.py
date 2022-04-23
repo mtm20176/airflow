32451dd790bb00900e2a271e4fe8f623511195cf
Mark Milligan <mtm20176@gmail.com>
2022-04-23 20:17:59 +0000

    DAG to read sets from json, analyze calltypes, merge 2021,2020

--- dags/datatest.py
+++ dags/datatest.py
@@ -20,40 +20,38 @@ default_args = {
 dag = DAG('datafile', default_args=default_args)
 
 
-def task_read13():
-  print("hello from task13 read data")
-  #query="https://data.colorado.gov/resource/tv8u-hswn.json"
-  data_2013=pd.read_json("https://data.policefoundation.org/resource/jhvd-4583.json")
-  data_2013.to_csv('/home/coder/data_2013.csv')
+def task_read_la_911_2021_data():
+  print("reading data from Los Angeles 911 activity")
+  la_911_data=pd.read_json("https://data.lacity.org/resource/cibt-wiru.json")
+  la_911_data.to_csv("../sets/la_911_2021_data.csv")
 
-def task_read12():
-  print("hello from task12 read data")
-  #query="https://data.colorado.gov/resource/tv8u-hswn.json"
-  data_2012=pd.read_json("https://data.policefoundation.org/resource/fgcx-vmf9.json")
-  data_2012.to_csv('/home/coder/data_2012.csv')
+def task_read_la_911_2020_data():
+  print("reading data from Los Angeles 911 activity")
+  la_911_data=pd.read_json("https://data.lacity.org/resource/84iq-i2r6.json")
+  la_911_data.to_csv("../sets/la_911_2020_data.csv")
 
  
 def task_merge():
-   print("tasks 2012")
-   data_2012=pd.read_csv('/home/coder/data_2012.csv')
-   year2012=data_2012['ward'].value_counts(sort=True, ascending=True).to_frame() #2012 arrests by ward
-   print("tasks 2013")
-   data_2013=pd.read_csv('/home/coder/data_2012.csv')
-   year2013=data_2013['ward'].value_counts(sort=True, ascending=True).to_frame() #2013 arrests by ward
-   m=pd.concat([year2012,year2013],axis=1)
-   m.columns=['year2012','year2013']
-   m.to_csv('/home/coder/mdata.csv')
+   print("2021 911 call types")
+   data_2021=pd.read_csv('../sets/la_911_2021_data.csv')
+   calltype_2021=data_2021['call_type_text'].value_counts(sort=True, ascending=True).to_frame()
+   print("2020 911 call types")
+   data_2020=pd.read_csv('../sets/la_911_2020_data.csv')
+   calltype_2020=data_2021['call_type_text'].value_counts(sort=True, ascending=True).to_frame()
+   m=pd.concat([calltype_2021,calltype_2020],axis=1)
+   m.columns=['year2021','year2020']
+   m.to_csv('../sets/la_911_2021_2020_calltypes.csv')
 
 
 t1 = BashOperator(
-    task_id='read_json_2012',
-    python_callable=task_read12(),
+    task_id='read_json_2021',
+    python_callable=task_read_la_911_2021_data(),
     bash_command='python3 ~/airflow/dags/datatest.py',
     dag=dag)
 
 t2 = BashOperator(
-    task_id='read_json_2013',
-    python_callable=task_read13(),
+    task_id='read_json_2020',
+    python_callable=task_read_la_911_2020_data(),
     bash_command='python3 ~/airflow/dags/datatest.py',
     dag=dag)
 
